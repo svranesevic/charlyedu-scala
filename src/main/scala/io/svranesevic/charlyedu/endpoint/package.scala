64b7b22ac316b5c54bf1e1c0659d33fcabdb8257
svranesevic <svranesevic@users.noreply.github.com>
2019-09-28 12:45:25 +0200

    Add constraint on date time parameters to be in the past or present

--- src/main/scala/io/svranesevic/charlyedu/endpoint/package.scala
+++ src/main/scala/io/svranesevic/charlyedu/endpoint/package.scala
@@ -1,13 +1,24 @@
 package io.svranesevic.charlyedu
 
-import java.time.ZonedDateTime
+import java.time.{ ZoneId, ZonedDateTime }
 
-import tapir.{ query, EndpointInput }
+import tapir.{ query, EndpointInput, Validator }
 
 package object endpoint {
 
   import io.svranesevic.charlyedu.codec.Implicits._
 
-  val startDateParameter: EndpointInput.Query[ZonedDateTime] = query[ZonedDateTime]("start")
-  val endDateParameter: EndpointInput.Query[ZonedDateTime]   = query[ZonedDateTime]("end")
+  val startDateParameter: EndpointInput.Query[ZonedDateTime] =
+    query[ZonedDateTime]("start")
+      .validate(
+        Validator.custom(time => time.isBefore(ZonedDateTime.now(ZoneId.of("UTC"))),
+                         "Date time must not be in the future")
+      )
+
+  val endDateParameter: EndpointInput.Query[ZonedDateTime] =
+    query[ZonedDateTime]("end")
+      .validate(
+        Validator.custom(time => time.isBefore(ZonedDateTime.now(ZoneId.of("UTC"))),
+                         "Date time must not be in the future")
+      )
 }
